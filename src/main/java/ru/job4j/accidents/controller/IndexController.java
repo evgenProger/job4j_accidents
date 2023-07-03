81a70ecd325aa2d79e42c1ada231fd096ca655f8
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2023-07-03 20:13:31 +0300

    2. IndexController. Таблица и вид. [#2092 # [#2092]]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -3,13 +3,17 @@ package ru.job4j.accidents.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.repository.AccidentMemRepository;
 import ru.job4j.accidents.repository.AccidentRepository;
 
 @Controller
 public class IndexController {
 
-    private final AccidentRepository accidentRepository = AccidentMemRepository.getINSTANCE();
+    private final AccidentRepository accidentRepository;
+
+    public IndexController(AccidentRepository accidentRepository) {
+        this.accidentRepository = accidentRepository;
+    }
+
 
     @GetMapping("/")
     public String getAll(Model model) {
