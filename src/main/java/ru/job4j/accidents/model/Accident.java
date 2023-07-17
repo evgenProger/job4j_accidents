3ad1ba5deed528490f709806db007dc8b458958a
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2023-07-17 13:56:11 +0300

    4. Form с композиционным объектом [#305522  [#305522]]

--- src/main/java/ru/job4j/accidents/model/Accident.java
+++ src/main/java/ru/job4j/accidents/model/Accident.java
@@ -6,11 +6,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Setter
 public class Accident {
     @EqualsAndHashCode.Include
     private int id;
     private String name;
     private String text;
     private String address;
+    private AccidentType type;
+
+    public Accident(int id, String name, String text, String address) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+        this.address = address;
+    }
 }
